4086aa04dfb1e50b6e108a403af5d00a2d53f861
Tom Fleet <tomfleet@gmail.com>
2014-12-11 12:04:54 +0000

    Added RGB Fade along strip
    Displays a RGB rainbow along the length of the strip, as defined by
    "numLEDs".

--- AVR_APA102/AVR_APA102/AVR_APA102.cpp
+++ AVR_APA102/AVR_APA102/AVR_APA102.cpp
@@ -11,12 +11,12 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-//include the external "C" APA102 funcitons
+// include the external "C" APA102 funcitons
 extern "C" {
 	#include "light_apa102.h"
 	}
 	
-//include the RGBConverter class
+// include the RGBConverter class
 #include "RGBConverter.h"
 
 // Number of LEDs
@@ -28,28 +28,46 @@ struct cRGB ledFrame[numLEDs];
 // uint8_t array for doing RGB/HSV calcs.
 uint8_t rgb[3] = {0,0,0};
 
-
 int main(void)
 {
 	// new RGB converter class
 	RGBConverter rgbConv;
 	
 	// saturation and variance 	
-	double sat= 1.0;
+	double sat = 1.0;
 	double var = 1.0;
 	while(1)
-	{		
-		for(float rotation = 0; rotation < 1; rotation += 0.01)	//cycle round the hue wheel
+	{
+		// rainbow fade along strip
+		for(int count = 0; count < numLEDs; count +=1)
+		{
+			double ledHue = (1.0/numLEDs)*count;
+			rgbConv.hsvToRgb(ledHue, sat, var, rgb);
+			ledFrame[count].r = rgb[0];
+			ledFrame[count].g = rgb[1];
+			ledFrame[count].b = rgb[2];
+		}
+		apa102_setleds(ledFrame, numLEDs);
+		
+		while(1)
 			{
-				rgbConv.hsvToRgb(rotation, sat, var, rgb); //do rgb conv based on current hue, sat and var.
+				;; // stop here
+			}
+		
+		// rainbow cycle through all pixels
+		for(float rotation = 0; rotation < 1; rotation += 0.01)	// cycle round the hue wheel
+			{
+				rgbConv.hsvToRgb(rotation, sat, var, rgb); // do rgb conv based on current hue, sat and var.
 				for(int ledCount = 0; ledCount < numLEDs; ledCount+=1)	
 					{
-						//paint the entire strip with the RGB from the current HSV
-						ledFrame[ledCount].r=rgb[0];ledFrame[ledCount].g=rgb[1];ledFrame[ledCount].b=rgb[2];    // Write red to array
+						// paint the entire strip with the RGB from the current HSV
+						ledFrame[ledCount].r=rgb[0];
+						ledFrame[ledCount].g=rgb[1];
+						ledFrame[ledCount].b=rgb[2];    
 					}
-				//push the LED frame out to the LEDs
+				// push the LED frame out to the LEDs
 				apa102_setleds(ledFrame,numLEDs);
-				//slow down a bit, so we can actually see something other than a flicker!
+				// slow down a bit, so we can actually see something other than a flicker!
 				_delay_ms(20);
 			}
 	}
